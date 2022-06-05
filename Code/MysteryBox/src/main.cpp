d1c9575d62f06f673c6eb7e126bb56e167163722
rene bakx <r.bakx@fontys.nl>
2022-06-05 19:42:11 +0200

    Updated FreeCAD design and code

--- Code/MysteryBox/src/main.cpp
+++ Code/MysteryBox/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
-
+#define ARDUINO_ESP32S2_DEV 1
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <TinyGPSPlus.h>
+#include "ESP32_ISR_Servo.h"
 #include <SoftwareSerial.h>
 
 #define LED_BUILTIN 10
@@ -18,14 +19,26 @@
 #define MYSTERY_YEAR 2022
 #define MYSTERY_MONTH 6
 #define MYSTERY_DAY 4
-#define MYSTERY_HOUR 23
+#define MYSTERY_HOUR 17
+#define MAX_TIME_POWER_ON_MS 120000
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library.
 
+#define RELAIS_PIN_1 6
+#define RELAIS_PIN_2 7
+#define SERVO_PIN 13
+#define SERVO_OPEN 80
+#define SERVO_CLOSE 0
+#define BUZZER_PIN 15
+#define BUZZER_CHANNEL 1
+
 #define OLED_RESET 18 // Reset pin # (or -1 if sharing Arduino reset pin)
 // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 #define SleftEEN_ADDRESS 0x3C
+
+const double TARGET_LAT = 51.39482540756363, TARGET_LON = 5.479256311543883;
+
 Adafruit_SSD1306 display(SleftEEN_WIDTH, SleftEEN_HEIGHT, &Wire, OLED_RESET);
 
 static const int RXPin = 4, TXPin = 3;
@@ -93,7 +106,7 @@ void displayInfo()
   Serial.println();
 }
 
-void InitializeDisplay()
+void InitDisplay()
 {
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, SleftEEN_ADDRESS))
@@ -104,7 +117,7 @@ void InitializeDisplay()
   }
 
   // display.setRotation(2); // Uncomment to rotate display 180 degrees
-  display.setTextSize(3);              // Normal 1:1 pixel scale
+  display.setTextSize(TEXT_SIZE);
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setTextWrap(false);
   display.cp437(true); // Use full 256 char 'Code Page 437' font
@@ -136,6 +149,15 @@ static void ReadGpsData()
   } while (millis() - start < GPS_GET_DATA_TIME_MS);
 }
 
+unsigned long distanceToTarget()
+{
+  return (unsigned long)TinyGPSPlus::distanceBetween(
+      gps.location.lat(),
+      gps.location.lng(),
+      TARGET_LAT,
+      TARGET_LON);
+}
+
 bool CheckIfDayHasCome(int year, int month, int day)
 {
 
@@ -144,65 +166,169 @@ bool CheckIfDayHasCome(int year, int month, int day)
   return currentDay >= dayToCheck;
 }
 
-bool CheckIfHourHasCome(int hour)
+bool CheckIfHourHasCome(int year, int month, int day, int hour)
 {
-  return gps.time.hour() >= hour;
+  unsigned long currentDayAndHour = gps.time.hour() + 100 * gps.date.day() + 10000 * gps.date.month() + 1000000 * gps.date.year();
+  unsigned long dayAndHourToCheck = hour + 100 * day + 10000 * month + 1000000 * year;
+  return currentDayAndHour >= dayAndHourToCheck;
+}
+
+void InitTone()
+{
+  ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
+  ledcWrite(0, 128); // 50% duty cycle
+}
+
+void PlayTone(int freq_hz, int duration_ms)
+{
+  ledcWriteTone(BUZZER_CHANNEL, freq_hz);
+  delay(duration_ms);
+}
+
+void PlayNote(char note, int duration_ms)
+{
+  char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',
+                  'c', 'd', 'e', 'f', 'g', 'a', 'b',
+                  'x', 'y'};
+
+  int tones[] = {261, 293, 329, 349, 392, 440, 493,
+                 523, 587, 659, 698, 783, 880, 987,
+                 783, 698};
+
+  int nofTones = sizeof(tones) / sizeof(int);
+
+  int SPEE = 5;
+
+  // play the tone corresponding to the note name
+
+  for (int i = 0; i < nofTones; i++)
+  {
+
+    if (names[i] == note)
+    {
+      int newduration_ms = duration_ms / SPEE;
+      PlayTone(tones[i], newduration_ms);
+    }
+  }
+}
+
+void NoTone()
+{
+  ledcWriteTone(BUZZER_CHANNEL, 0);
+}
+
+typedef struct
+{
+  char notes[64];
+  int beats[64];
+  int nofNotes;
+  int tempo;
+} Tune;
+
+enum tuneEnum
+{
+  Hello,
+  Happy,
+  HappyBirthday
+};
+
+Tune tuneArray[3] = {
+    {"CDEc", {2, 2, 2, 2}, 4, 100},
+    {"CEGcGc", {1, 1, 1, 2, 1, 6}, 6, 200},
+    {"GGAGcB GGAGdc GGxecBA yyecdc", {2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 8, 16, 1, 2, 2, 8, 8, 8, 16}, 28, 200},
+};
+
+void PlayTune(Tune tune)
+{
+  for (int i = 0; i < tune.nofNotes; i++)
+  {
+    if (tune.notes[i] == ' ')
+    {
+      delay(tune.beats[i] * tune.tempo); // delay between notes
+    }
+    else
+    {
+      PlayNote(tune.notes[i], tune.beats[i] * tune.tempo);
+    }
+    // time delay between notes
+    delay(tune.tempo);
+  }
+  NoTone();
 }
 
 void setup()
 {
+  delay(2000);
   Serial.begin(9600);
   Serial.println("Setup started!");
 
-  pinMode(6, OUTPUT_OPEN_DRAIN);
-  pinMode(7, OUTPUT_OPEN_DRAIN);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
+  pinMode(RELAIS_PIN_1, OUTPUT_OPEN_DRAIN);
+  pinMode(RELAIS_PIN_2, OUTPUT_OPEN_DRAIN);
+  digitalWrite(RELAIS_PIN_1, LOW);
+  digitalWrite(RELAIS_PIN_2, LOW);
+
+  InitTone();
+  PlayTune(tuneArray[Hello]);
+
+  int servoIndex = ESP32_ISR_Servos.setupServo(SERVO_PIN, 1000, 2000);
+  ESP32_ISR_Servos.setPosition(servoIndex, SERVO_CLOSE);
 
   ss.begin(GPSBaud);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  InitializeDisplay();
+  InitDisplay();
 
-  ReadGpsData();
-  if (gps.date.isValid() && gps.time.isValid())
+  int tmpDate = gps.date.year();
+  double tmpLocation = gps.location.lat();
+  unsigned long startTime = millis();
+  unsigned long currentTime;
+  bool finished = false;
+  do
   {
-    if (!CheckIfDayHasCome(MYSTERY_YEAR, MYSTERY_MONTH, MYSTERY_DAY))
+    currentTime = millis();
+    ReadGpsData();
+    if (gps.date.isValid() && gps.time.isValid() && gps.date.year() > 2020)
     {
-      sprintf(messageBuf, "Het is vandaag nog geen tijd, probeer het morgen nog eens!");
-      ShowTextOnDisplay(messageBuf);
-    }
-    else
-    {
-      if (!CheckIfHourHasCome(MYSTERY_HOUR))
+      if (!CheckIfDayHasCome(MYSTERY_YEAR, MYSTERY_MONTH, MYSTERY_DAY))
       {
-        sprintf(messageBuf, "Vandaag is de dag, neem me mee!");
+        sprintf(messageBuf, "Het is vandaag nog geen tijd, probeer het morgen nog eens!");
         ShowTextOnDisplay(messageBuf);
+        finished = true;
       }
       else
       {
-        if (gps.location.isValid())
+        if (!CheckIfHourHasCome(MYSTERY_YEAR, MYSTERY_MONTH, MYSTERY_DAY, MYSTERY_HOUR))
         {
-          sprintf(messageBuf, "De afstand tot het target is 12345 meter!");
+          sprintf(messageBuf, "Vandaag is de dag, neem me mee!");
           ShowTextOnDisplay(messageBuf);
+          finished = true;
         }
         else
         {
-          sprintf(messageBuf, "Neem de Mysterybox mee naar buiten!");
-          ShowTextOnDisplay(messageBuf);
+          if (gps.location.isValid() && gps.location.lat() != 0.0)
+          {
+            sprintf(messageBuf, "De afstand tot het target is %d meter!", distanceToTarget());
+            ShowTextOnDisplay(messageBuf);
+            finished = true;
+          }
+          else
+          {
+            sprintf(messageBuf, "Neem de Mysterybox mee naar buiten!");
+            ShowTextOnDisplay(messageBuf);
+          }
         }
       }
     }
-  }
-  else
-  {
-    sprintf(messageBuf, "Neem de Mysterybox mee naar buiten!");
-    ShowTextOnDisplay(messageBuf);
-  }
+    else
+    {
+      sprintf(messageBuf, "Neem de Mysterybox mee naar buiten!");
+      ShowTextOnDisplay(messageBuf);
+    }
+  } while (currentTime - startTime < MAX_TIME_POWER_ON_MS && finished == false);
 
   // Switch off power relais.
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
+  digitalWrite(RELAIS_PIN_1, HIGH);
+  digitalWrite(RELAIS_PIN_2, HIGH);
 
   Serial.println("Setup finished!");
 }
@@ -213,11 +339,6 @@ void loop()
   delay(100);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(100);
-
-  ReadGpsData();
-  if (gps.date.isValid() && gps.time.isValid())
-  {
-    sprintf(messageBuf, "hour: %d", gps.time.hour());
-    ShowTextOnDisplay(messageBuf);
-  }
+  Serial.println("year: " + String(gps.date.year()));
+  Serial.println("lat: " + String(gps.location.lat()));
 }
